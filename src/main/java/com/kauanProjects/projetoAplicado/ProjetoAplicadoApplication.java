c27192eafd9fd4a543a863e9dd028686a47c57df
Kauan-Code19 <pkauprofissional@gmail.com>
2024-11-28 15:27:33 -0300

    projeto criado, gitignore.

---
+++ src/main/java/com/kauanProjects/projetoAplicado/ProjetoAplicadoApplication.java
@@ -0,0 +1,13 @@
+package com.kauanProjects.projetoAplicado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAplicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAplicadoApplication.class, args);
+	}
+
+}
