e11278e63899bc8e5a12054b679c7651adb34af2
Kauan-Code19 <pkauprofissional@gmail.com>
2024-12-03 15:00:02 -0300

    Ajuste na relação de district com ecopoint, houve a necessidade de trocar one to one para many to one

--- src/main/java/com/kauanProjects/projetoAplicado/dtos/DistrictResponseDTO.java
+++ src/main/java/com/kauanProjects/projetoAplicado/dtos/DistrictResponseDTO.java
@@ -1,6 +1,7 @@
 package com.kauanProjects.projetoAplicado.dtos;
 
 import com.kauanProjects.projetoAplicado.entities.District;
+import com.kauanProjects.projetoAplicado.entities.Ecopoint;
 import lombok.Getter;
 import java.util.List;
 
@@ -9,13 +10,14 @@ public class DistrictResponseDTO {
 
     private final Long id;
     private final String name;
-    private final String ecopoint;
+    private final List<String> ecopoint;
     private final List<CollectResponseDTO> collects;
 
     public DistrictResponseDTO(District district) {
         id = district.getId();
         name = district.getName();
-        ecopoint = (district.getEcopoint() != null) ? district.getEcopoint().getAddress() : null;
+        ecopoint = (district.getEcopoints() != null)
+                ? district.getEcopoints().stream().map(Ecopoint::getAddress).toList() : null;
         collects = district.getCollects().stream().map(CollectResponseDTO::new).toList();
     }
 }
